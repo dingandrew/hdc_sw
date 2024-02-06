94f1b4b9166cc7798c5b6d2669479bcdd3953c50
Eric Chan <eachan10@gmail.com>
2024-02-06 12:15:18 -0800

    Avoid name collision with cmath

--- c_src/includes/fasthd.h
+++ c_src/includes/fasthd.h
@@ -1,5 +1,5 @@
 #include <math.h>
 
-float cos(float* x, float* xs, float* out, int x_size, int xs_size);
+float fasthd_cos(float* x, float* xs, float* out, int x_size, int xs_size);
 
 float* onepass(float* x, float* y, float* m, int n, int c, float lr, int x_size);
